5e11bbb06df94bdcddc284e8d40655fe142cbe36
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-10-24 22:36:27 +0200

    Rename

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-rdp-tools",
+    name: "SwiftRDPTools",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-rdp-tools",
-            targets: ["swift-rdp-tools"]
+            name: "RDPTools",
+            targets: ["RDPTools"]
         )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-rdp-tools"
+            name: "RDPTools"
         )
     ]
 )
